6c4a9ab84a3d8348439752196ae6fc66da3737e8
Nano Taboada <nanotaboada@msn.com>
2017-03-07 17:00:29 -0300

    Added CORS support.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
+var cors = require('cors');
 var express = require('express');
 var path = require('path');
 var builder = require('./book/builder.js');
@@ -14,6 +15,7 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 /* --------------------------------------------------------------------------
